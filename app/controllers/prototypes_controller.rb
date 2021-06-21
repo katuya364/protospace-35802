082427ab1a885818f780039a20cdc9927e3ec098
katuya364 <konishi8439@gmail.com>
2021-06-21 21:40:04 +0900

    ログアウト時にプロトタイプの詳細ページに遷移しないことの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_prototype, only: [:edit, :show]
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index, :show]
 
 
   def index
@@ -24,11 +23,13 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
     unless @prototype.user_id == current_user.id
       redirect_to action: :index
     end
@@ -53,10 +54,6 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
   end
 
   def move_to_index
